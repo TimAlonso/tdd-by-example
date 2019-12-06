58949aded550c444ecb8c749135756730e2a7b66
Tim Alonso <tim.alonso@moonpig.com>
2019-12-06 14:30:24 +0000

    Factory method for subclasses - green

--- MultiCurrency.TestDriven.Tests/Tests.cs
+++ MultiCurrency.TestDriven.Tests/Tests.cs
@@ -7,27 +7,27 @@ namespace MultiCurrency.TestDriven.Tests
         [Test]
         public void TestMultiplication()
         {
-            Dollar five = new Dollar(5);
-            Assert.That(five.Times(2), Is.EqualTo(new Dollar(10)));
-            Assert.That(five.Times(3), Is.EqualTo(new Dollar(15)));
+            Money five = Money.Dollar(5);
+            Assert.That(five.Times(2), Is.EqualTo(Money.Dollar(10)));
+            Assert.That(five.Times(3), Is.EqualTo(Money.Dollar(15)));
         }
 
         [Test]
         public void TestFrancMultiplication()
         {
-            Franc five = new Franc(5);
-            Assert.That(five.Times(2), Is.EqualTo(new Franc(10)));
-            Assert.That(five.Times(3), Is.EqualTo(new Franc(15)));
+            Franc five = Money.Franc(5);
+            Assert.That(five.Times(2), Is.EqualTo(Money.Franc(10)));
+            Assert.That(five.Times(3), Is.EqualTo(Money.Franc(15)));
         }
 
         [Test]
         public void TestEquality()
         {
-            Assert.That(new Dollar(5).Equals(new Dollar(5)), Is.True);
-            Assert.That(new Dollar(5).Equals(new Dollar(6)), Is.False);
-            Assert.That(new Franc(5).Equals(new Franc(5)), Is.True);
-            Assert.That(new Franc(5).Equals(new Franc(6)), Is.False);
-            Assert.That(new Dollar(5).Equals(new Franc(5)), Is.False);
+            Assert.That(Money.Dollar(5).Equals(Money.Dollar(5)), Is.True);
+            Assert.That(Money.Dollar(5).Equals(Money.Dollar(6)), Is.False);
+            Assert.That(Money.Franc(5).Equals(Money.Franc(5)), Is.True);
+            Assert.That(Money.Franc(5).Equals(Money.Franc(6)), Is.False);
+            Assert.That(Money.Dollar(5).Equals(Money.Franc(5)), Is.False);
         }
     }
 }
--- MultiCurrency.TestDriven/Money.cs
+++ MultiCurrency.TestDriven/Money.cs
@@ -1,8 +1,20 @@
 ﻿namespace MultiCurrency.TestDriven
 {
-    public class Money
+    public abstract class Money
     {
         public int Amount;
+
+        public abstract Money Times(int multiplier);
+
+        public static Dollar Dollar(int amount)
+        {
+            return new Dollar(amount);
+        }
+
+        public static Franc Franc(int amount)
+        {
+            return new Franc(amount);
+        }
 
         public override bool Equals(object obj)
         {
