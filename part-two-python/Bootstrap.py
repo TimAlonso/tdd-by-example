5e6cd68ca0b0edbd1b720ee55620dbcd7beed290
Tim Alonso <tim.alonso@moonpig.com>
2020-01-15 22:13:04 +0000

    Test result failed formatting - red

--- part-two-python/Bootstrap.py
+++ part-two-python/Bootstrap.py
@@ -42,7 +42,7 @@ class TestResult:
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
-        result = test.run()
+        test.run()
         assert("setUp testMethod tearDown " == test.log)
     
     def testResult(self):
@@ -53,6 +53,12 @@ class TestCaseTest(TestCase):
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
         result = test.run()
+        assert("1 run, 1 failed" == result.summary())
+
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
         assert("1 run, 1 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
