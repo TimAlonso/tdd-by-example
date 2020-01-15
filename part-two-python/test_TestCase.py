b7123f25497fdb95759ab07178ff8a58710fe82a
Tim Alonso <tim.alonso@moonpig.com>
2020-01-15 12:30:23 +0000

    Test case for setup - red

--- part-two-python/test_TestCase.py
+++ part-two-python/test_TestCase.py
@@ -8,3 +8,8 @@ class test_TestCase(unittest.TestCase):
         self.assertEqual(test.wasRun, None)
         test.run()
         self.assertEqual(test.wasRun, 1)
+    
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        self.assertEqual(test.wasSetUp, 1)
