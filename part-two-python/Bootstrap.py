cb4171d2ad1ce9489848b51a56de5f493e8eb495
Tim Alonso <tim.alonso@moonpig.com>
2020-01-15 13:00:51 +0000

    Invoke setup first - green

--- part-two-python/Bootstrap.py
+++ part-two-python/Bootstrap.py
@@ -2,7 +2,11 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
@@ -13,6 +17,9 @@ class WasRun(TestCase):
 
     def testMethod(self):
         self.wasRun = 1
+
+    def setUp(self):
+        self.wasSetUp = 1
 
 class TestCaseTest(TestCase):
     def testRunning(self):
