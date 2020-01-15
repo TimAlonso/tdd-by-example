f76263e293a3d514e5cf7674ee9be610a7b36dbb
Tim Alonso <tim.alonso@moonpig.com>
2020-01-15 13:15:03 +0000

    Refactor test - green

--- part-two-python/Bootstrap.py
+++ part-two-python/Bootstrap.py
@@ -16,16 +16,19 @@ class WasRun(TestCase):
 
     def testMethod(self):
         self.wasRun = 1
+        self.log = self.log + "testMethod "
 
     def setUp(self):
         self.wasRun = None
         self.wasSetUp = 1
-        self.log = "setUp"
+        self.log = "setUp "
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testTemplateMethod(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert("setUp testMethod " == test.log)
+        self.test.run()
+        assert("setUp testMethod " == self.test.log)
 
 TestCaseTest("testTemplateMethod").run()
